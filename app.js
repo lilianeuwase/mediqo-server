0a91b3efd27d9297a712380f1b02ebac125f8ae6
lilianeuwase <38309956+lilianeuwase@users.noreply.github.com>
2023-05-18 16:14:43 +0200

    18-May commit

--- app.js
+++ app.js
@@ -139,7 +139,8 @@ app.post("/forgot-password", async (req, res) => {
       expiresIn: "20m",
     });
     // const link = `https://mediqo-api.onrender.com/reset-password/${oldUser._id}/${token}`;
-    const link = `http://localhost:5000/reset-password/${oldUser._id}/${token}`;
+    // const link = `http://localhost:5000/reset-password/${oldUser._id}/${token}`;
+    const link = `https://fantastic-python.cyclic.app/reset-password/${oldUser._id}/${token}`;
     var transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
